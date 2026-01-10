fd9307f6925c4d699f4713e2b7f0092a0765a070
MonuJangra-git <jangramonu908@gmail.com>
2026-01-11 02:00:05 +0530

    2nd Update nmap scanner main.py

--- nmap scanner/main.py
+++ nmap scanner/main.py
@@ -14,7 +14,8 @@ if resp == '1':
     print(scanner.scaninfo())
     print("ip status is :- ", scanner[ip_addrs].state())
     print(scanner[ip_addrs].all_protocols())
-    print(scanner[ip_addrs]['tcp'].keys())
+    if 'tcp' in scanner[ip_addrs]:
+        print(scanner[ip_addrs]['tcp'].keys())
 
 elif resp == '2':
     print(nmap.__version__)
@@ -22,18 +23,23 @@ elif resp == '2':
     print(scanner.scaninfo())
     print("ip status is :- ", scanner[ip_addrs].state())
     print(scanner[ip_addrs].all_protocols())
-    print(scanner[ip_addrs]['udp'].keys()) 
+    if 'tcp' in scanner[ip_addrs]:
+        print(scanner[ip_addrs]['udp'].keys()) 
 elif resp == '3':
     print(nmap.__version__)
-    (scanner.scan(ip_addrs,'1-2000','-sU -v -A -O '))
+    (scanner.scan(ip_addrs,'1-2000','-sS -v -A -O '))
     print(scanner.scaninfo())
     print("ip status is :- ", scanner[ip_addrs].state())
     print(scanner[ip_addrs].all_protocols())
-    print(scanner[ip_addrs]['tcp'].keys())
+    if 'tcp' in scanner[ip_addrs]:
+        print(scanner[ip_addrs]['tcp'].keys())
 else :
     print("please enter correct option")
+    sys.exit()
 
-
+with open("scan_results.txt", "a") as f:
+    f.write(str(scanner[ip_addrs]))
+print("scan results saved in scan_results.txt")
 
 
 
